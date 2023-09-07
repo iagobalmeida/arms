a8a7ff67ad49a91eb99c349df77e3d8ce00cd933
Iago <iago.almeida@visie.com.br>
2023-09-07 18:48:32 -0300

    ✨ Criação seção "projetar" / Criação seção "conta" / Ajuste menu de navegação para lateral esquerda de redes sociais

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,21 @@
 module.exports = {
-    content: [
-      "./*.html"
-    ],
-    theme: {
-      extend: {
-        colors: {
-          'arms-black': '#201F1F',
-          'arms-white': '#FFFFFF',
-          'arms-gray': '#E0E0E0',
-          'arms-light-gray': '#535353',
-          'arms-yellow': '#F9C406',
-          'arms-blue': '#6506F9',
-          'arms-purple': '#2A0E47',
-          'arms-blue-dark': '#4604AE',
-        },
-        fontSize: {
-          '8.5xl': '100px'
-        }
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "arms-black": "#201F1F",
+        "arms-white": "#FFFFFF",
+        "arms-gray": "#E0E0E0",
+        "arms-light-gray": "#535353",
+        "arms-yellow": "#F9C406",
+        "arms-blue": "#6506F9",
+        "arms-purple": "#2A0E47",
+        "arms-blue-dark": "#4604AE",
+      },
+      fontSize: {
+        "8.5xl": "100px",
       },
     },
-    plugins: [],
-  }
-  
-  
+  },
+  plugins: [],
+};
