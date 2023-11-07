4cd8c741ce3877742c06654134c588a02083dfe0
Iago <iago.almeida@visie.com.br>
2023-11-07 14:13:25 -0300

    ✨ Finalizando animacao abas / adicionando redirecionamentos

--- assets/js/tabs_criamos.js
+++ assets/js/tabs_criamos.js
@@ -3,39 +3,34 @@ const querySelectorAllEventListener = (query, event, callback) => {
         el.addEventListener(event, (ev) => {
             callback(el, ev)
         });
-    })
+    });
 }
 
 window.addEventListener('load', () => {
     let currentTab = 0;
 
+    const tabIndicator = document.querySelector('.tab-indicator');
+    const tabIndicatorClassList = [...tabIndicator.classList];
+    const tabClasses = {
+        active: ['text-arms-black', 'active'],
+        inactive: ['text-arms-white', 'inactive']
+    }
+
     const setCurrentTab = (tab) => {
         currentTab = tab;
-        document.querySelectorAll('[data-tab-parent]').forEach(el => {
-            el.classList.add('hidden');
-        });
-        document.querySelectorAll(`[data-tab-parent="${currentTab}"]`).forEach(el => {
-            el.classList.remove('hidden');
-        });
+        document.querySelectorAll('[data-tab-parent]').forEach(el => el.classList.add('hidden'));
+        document.querySelectorAll(`[data-tab-parent="${currentTab}"]`).forEach(el => el.classList.remove('hidden'));
 
-        const tabClasses = {
-            active: ['text-arms-black', 'bg-arms-white'],
-            inactive: ['text-arms-white', 'bg-arms-black', 'hover:bg-arms-black-600']
-        }
+        tabIndicator.classList = [...tabIndicatorClassList, `on-${currentTab}`].join(' ');
 
-        const getTabClasses = (val) => {
-            return val == tab ? tabClasses.active : tabClasses.inactive
-        }
+        const getTabClasses = (val) => (val == tab ? tabClasses.active : tabClasses.inactive);
 
         document.querySelectorAll('[data-tab-toggle]').forEach((el, elIndex) => {
-            ['hover:bg-arms-black-600', 'text-arms-black', 'text-arms-white', 'bg-arms-white', 'bg-arms-black'].forEach(_class => {
+            [...tabClasses['active'], ...tabClasses['inactive']].forEach(_class => {
                 el.classList.remove(_class);
             });
-
             const classes = getTabClasses(elIndex);
             classes.forEach(_class => el.classList.add(_class));
-
-
             el.querySelector('svg').classList.remove('rotate-90');
             if(elIndex == currentTab) el.querySelector('svg').classList.add('rotate-90');
         })
